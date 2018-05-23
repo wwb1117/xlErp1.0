8533f4457f9a54175ff1d438fe081b650beefac6
liyike <978795746@qq.com>
2018-05-23 15:52:21 +0800

    lyk

---
+++ ERP1.0/src/views/work/authority.vue
@@ -0,0 +1,56 @@
+<template>
+    <section :style="{height: $store.state.home.modelContentHeight + 'px'}">
+        <!-- 顶部 -->
+        <header class="authority_top">
+            <p class='authority_title'>权限管理</p>
+        </header>
+        <section class="authority_conent" :style="{height: $store.state.home.modelContentHeight + 'px'}">
+            <div class="authority_box" :style="{height: $store.state.home.modelContentHeight-20 + 'px'}">
+
+            </div>
+        </section>
+        <footer class="authority_bottom">
+            <el-button type="primary" size='small' style="margin:10px 0 0 30px">完成</el-button>
+        </footer>
+    </section>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+/* 顶部 */
+.authority_top{
+    display: flex;
+    justify-content: space-between;
+    height: 46px;
+    padding: 10px;
+    border-bottom: 1px solid #e5e8e8
+}
+.authority_title{
+    height: 25px;
+    line-height: 25px;
+    margin-left: 20px;
+    font-size: 15px;
+    color: #5e6161
+}
+
+/* 内容 */
+.authority_conent{
+    padding: 10px;
+    background: #f5f5f5
+}
+.authority_box{
+    border: 1px solid #e6e9eb;
+    color: #5e6161;
+    background: white
+}
+/* 底部 */
+.authority_bottom{
+    border-top:1px solid #e5e8e8;
+    position: relative;
+
+    background: white;
+}
+</style>
---
+++ ERP1.0/src/views/work/user/user.vue
@@ -0,0 +1,56 @@
+<template>
+    <section :style="{height: $store.state.home.modelContentHeight + 'px'}">
+        <!-- 顶部 -->
+        <header class="user_top">
+             <el-tabs v-model="activeName" >
+                <el-tab-pane label="用户列表" name="first">
+                    <userlist></userlist>
+                </el-tab-pane>
+                <el-tab-pane label="权限组" name="second">
+                    <autohority></autohority>
+                </el-tab-pane>
+            </el-tabs>
+            <el-button type='primary' size='small'>新增用户</el-button>
+        </header>
+
+    </section>
+</template>
+<script>
+import userlist from 'views/work/user/userList'
+import autohority from 'views/work/user/authorityList'
+
+export default {
+    name: 'user',
+    data() {
+        return {
+            activeName: 'first'
+        }
+    },
+    methods: {
+
+    },
+    components: {
+        userlist,
+        autohority
+    }
+}
+</script>
+<style scoped>
+/* 顶部 */
+.user_top{
+    display: flex;
+    justify-content: space-between;
+    height: 46px;
+    padding: 8px 10px;
+    border-bottom: 1px solid #e5e8e8
+}
+.user_title{
+    height: 25px;
+    line-height: 25px;
+    margin-left: 20px;
+    font-size: 15px;
+    color: #5e6161
+}
+
+
+</style>
